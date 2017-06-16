a43a30755bdce0ef20b932aeab01c003fa2e0d62
Nagaraj Bhat <Nagaraj.Bhat@hitachiconsulting.com>
2017-06-16 16:01:30 +0530

    First Commit

---
+++ HProject.Web/Infrastructure/Extensions/EntitiesExtensions.cs
@@ -0,0 +1,27 @@
+﻿using HProject.Entities;
+using HProject.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HProject.Web.Infrastructure.Extensions
+{
+    public static class EntitiesExtensions
+    {
+
+        public static void UpdateProject(this Project project, ProjectViewModel projectVm)
+        {
+            project.ProjectName = projectVm.ProjectName;
+            project.ProjectManager = projectVm.ProjectManager;
+            project.ProjectEstimatedStartDate = projectVm.ProjectEstimatedStartDate;
+            project.ProjectEstimatedEndDate = projectVm.ProjectEstimatedEndDate;
+            project.ProjectActualStartDate = (projectVm.ProjectActualStartDate.HasValue ?  projectVm.ProjectActualStartDate: null);
+            project.ProjectActualEndDate = (projectVm.ProjectActualEndDate.HasValue ? projectVm.ProjectActualEndDate : null);
+            project.Email = projectVm.Email;
+            project.CreatedDate = DateTime.Now;
+
+
+        }
+    }
+}
